a3d85febfb53259c6337d90d97ab5c89b169eecd
Martin Wan <mlwan@uvic.ca>
2016-05-07 17:58:25 -0700

    added the ugly fish and end game scene

---
+++ Ugly Fish/EndScene.swift
@@ -0,0 +1,13 @@
+//
+//  EndScene.swift
+//  Ugly Fish
+//
+//  Created by Martin  on 2016-05-07.
+//  Copyright © 2016 Martin . All rights reserved.
+//
+
+import Cocoa
+
+class EndScene: SKScene {
+
+}
